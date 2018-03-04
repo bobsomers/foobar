34ad13daa7e0fd52e22fbd9abc78605a16cc07a1
Bob Somers <bob@bobsomers.com>
2018-03-04 00:22:17 -0800

    Linux build.

--- glfw/build.rs
+++ glfw/build.rs
@@ -6,29 +6,32 @@ use std::collections::HashMap;
 fn main() {
     let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
 
-    let mut config: HashMap<_, _> = [
-        ("_GLFW_X11", "0"),
-        ("_GLFW_WIN32", "0"),
-        ("_GLFW_COCOA", "0"),
-        ("_GLFW_WAYLAND", "0"),
-        ("_GLFW_MIR", "0"),
-        ("_GLFW_BUILD_DLL", "0"),
-        ("_GLFW_VULKAN_STATIC", "0"),
-        ("_GLFW_USE_HYBRID_HPG", "0"),
-        ("_GLFW_HAS_XF86VM", "0"),
-        ("_GLFW_USE_CHDIR", "0"),
-        ("_GLFW_USE_MENUBAR", "0"),
-        ("_GLFW_USE_RETINA", "0"),
-    ].iter().cloned().collect();
+    // Platform defines include:
+    //      _GLFW_X11
+    //      _GLFW_WIN32
+    //      _GLFW_COCOA
+    //      _GLFW_WAYLAND
+    //      _GLFW_MIR
+    //      _GLFW_BUILD_DLL
+    //      _GLFW_VULKAN_STATIC
+    //      _GLFW_USE_HYRBID_HPG
+    //      _GLFW_HAS_XF86VM
+    //      _GLFW_USE_CHDIR
+    //      _GLFW_USE_MENUBAR
+    //      _GLFW_USE_RETINA
+
+    let mut config = HashMap::new();
 
     if target_os == "macos" {
-        *config.entry("_GLFW_COCOA").or_insert("0") = "1";
-        *config.entry("_GLFW_USE_CHDIR").or_insert("0") = "1";
-        *config.entry("_GLFW_USE_MENUBAR").or_insert("0") = "1";
-        *config.entry("_GLFW_USE_RETINA").or_insert("0") = "1";
+        config.insert("_GLFW_COCOA", "1");
+        config.insert("_GLFW_USE_CHDIR", "1");
+        config.insert("_GLFW_USE_MENUBAR", "1");
+        config.insert("_GLFW_USE_RETINA", "1");
+    } else if target_os == "linux" {
+        config.insert("_GLFW_X11", "1");
+        config.insert("_GLFW_HAS_XF86VM", "1");
     }
-
-    // TODO: Linux/Windows options
+    // TODO: Windows options
 
     let common_srcs = vec![
         "glfw-3.2.1/src/context.c",
@@ -49,7 +52,18 @@ fn main() {
             "glfw-3.2.1/src/posix_tls.c",
             "glfw-3.2.1/src/nsgl_context.m",
         ],
-        // TODO: Linux/Windows platform sources.
+        "linux" => vec![
+            "glfw-3.2.1/src/x11_init.c",
+            "glfw-3.2.1/src/x11_monitor.c",
+            "glfw-3.2.1/src/x11_window.c",
+            "glfw-3.2.1/src/xkb_unicode.c",
+            "glfw-3.2.1/src/linux_joystick.c",
+            "glfw-3.2.1/src/posix_time.c",
+            "glfw-3.2.1/src/posix_tls.c",
+            "glfw-3.2.1/src/glx_context.c",
+            "glfw-3.2.1/src/egl_context.c",
+        ],
+        // TODO: Windows platform sources.
         _ => panic!("Target OS '{}' not supported by GLFW.", target_os),
     };
 
@@ -71,5 +85,13 @@ fn main() {
         println!("cargo:rustc-link-lib=framework=CoreVideo");
         println!("cargo:rustc-link-lib=framework=IOKit");
         println!("cargo:rustc-link-lib=framework=OpenGL");
+    } else if target_os == "linux" {
+        println!("cargo:rustc-link-lib=dylib=rt");
+        println!("cargo:rustc-link-lib=dylib=m");
+        println!("cargo:rustc-link-lib=dylib=Xrandr");
+        println!("cargo:rustc-link-lib=dylib=Xinerama");
+        println!("cargo:rustc-link-lib=dylib=Xxf86vm");
+        println!("cargo:rustc-link-lib=dylib=Xcursor");
+        println!("cargo:rustc-link-lib=dylib=X11");
     }
 }
